89845ef5fb6a7a9f13c607c767cc0d1e54eda854
amalieasborg <amalieasborg@gmail.com>
2024-04-24 13:02:52 +0200

    Hej

---
+++ src/main/java/org/example/knockoffkatteprojekt/KnockoffKatteProjektApplication.java
@@ -0,0 +1,13 @@
+package org.example.knockoffkatteprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockoffKatteProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnockoffKatteProjektApplication.class, args);
+    }
+
+}
